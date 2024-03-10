68f3eaa508fadba91b5e0ee975aa2949a291bde8
WebGameCreator <138807724+WebGameCreator@users.noreply.github.com>
2024-03-10 12:10:46 +0100

    Improved Website
    - Added Luxair header and footer
    - Added Images to some of the answers
    - User Answers will be outputed at the end of the quiz

--- main.js
+++ main.js
@@ -1,52 +1,99 @@
-const questions = [
+const hotels = [
     {
-        question: "Where would you like to go?",
-        answers: [
-            { text: "City" },
-            { text: "Beach" },
-            { text: "Mountain" }
-        ]
+        place: [0],
+        people: [0],
+        activities: [0, 3]
     },
     {
-        question: "Who will go with you?",
-        answers: [
-            { text: "Alone" },
-            { text: "With another person" },
-            { text: "With my family" },
-            { text: "With friends" }
-        ]
+        place: [0, 1],
+        people: [0, 1, 2, 3],
+        activities: [1, 2, 3]
+    },
+    {
+        place: [2],
+        people: [0, 1],
+        activities: [0, 2]
     }
 ];
+
+const properties = {
+    place: {
+        question: "Where would you like to go?",
+        answers: [
+            { text: "City", image: "https://d1taxzywhomyrl.cloudfront.net/s3/ag-images-eu/09/1dd23cc06c31c31ba7df72f2c74db5bc.jpg?width=550&height=315&mode=crop" },
+            { text: "Beach", image: "https://www.luxairtours.lu/sites/default/files/styles/slide_image_xs_1px_480px_/public/2022-08/HDF_iStock-508035252_WEB%203%20-%20Big%20Teaser%20Image.jpg?itok=8iQdkiA6" },
+            { text: "Mountain", image: "images/mountains.jpeg" }
+        ]
+    },
+    people: {
+        question: "Who are you traveling with?",
+        answers: [
+            { text: "Alone", image: "" },
+            { text: "With another person", image: "" },
+            { text: "With my family", image: "" },
+            { text: "With friends", image: "" }
+        ]
+    },
+    activities: {
+        question: "What would you like to do?",
+        answers: [
+            { text: "Visit a museum", image: "" },
+            { text: "Visit a Zoo", image: "" },
+            { text: "Go to a cinema", image: "" },
+            { text: "Relax", image: "" }
+        ]
+    }
+};
 
 const answerContainer = document.getElementById("answerContainer");
 const questionElement = document.getElementById("questionElement");
 
+const userAnswers = {};
+
 let currentQuestion = 0;
+
 function displayQuestion() {
-    questionElement.innerHTML = questions[currentQuestion].question;
-    for (const answer of questions[currentQuestion].answers) {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.className = "answerButton";
-        button.onclick = function () {
-            for (const answerButton of document.querySelectorAll(".answerButton")) {
-                answerButton.parentElement.removeChild(answerButton);
+    const [property, { question, answers }] = Object.entries(properties)[currentQuestion];
+    questionElement.innerHTML = question;
+    for (let [index, answer] of answers.entries()) {
+
+        const answerButton = document.createElement("button");
+        answerButton.className = "answerButton";
+        answerButton.onclick = function () {
+            userAnswers[property] = index;
+            for (const element of document.querySelectorAll(".answerButton")) {
+                element.parentElement.removeChild(element);
             }
             currentQuestion++;
-            if (currentQuestion < questions.length) {
+            if (currentQuestion < Object.entries(properties).length) {
                 displayQuestion();
             }
             else {
-                questionElement.parentElement.removeChild(questionElement);
                 displayResult();
             }
         }
-        answerContainer.appendChild(button);
+
+        if (answer.image) {
+            const answerImage = document.createElement("img");
+            answerImage.className = "answerImage";
+            answerImage.src = answer.image;
+            answerButton.classList.add("imageAnswerButton");
+            answerButton.appendChild(answerImage)
+        }
+
+        const answerText = document.createElement("h2");
+        answerText.className = "answerText";
+        answerText.innerHTML = answer.text;
+        answerButton.appendChild(answerText);
+
+        answerContainer.appendChild(answerButton);
     }
 }
 
 function displayResult() {
-
+    questionElement.parentElement.removeChild(questionElement);
+    answerContainer.parentElement.removeChild(answerContainer);
+    alert(JSON.stringify(userAnswers));
 }
 
 displayQuestion();
