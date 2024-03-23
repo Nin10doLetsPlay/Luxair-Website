f473fa40ae02f17cb0e6613080ebf759daec9a06
WebGameCreator <138807724+WebGameCreator@users.noreply.github.com>
2024-03-23 17:09:57 +0100

    Added luxair destination api

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+
+async function get(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw response.status;
+        return await response.json();
+    } catch (error) {
+        console.error(`Get request to ${url} failed: ${error}`);
+        return null;
+    }
+}
+
+const { destinations } = await get("https://www.luxairtours.lu/luxair_destinations_overview");
+const data = [];
+for (const [key, destination] of Object.entries(destinations)) {
+    data.push({
+        id: destination.field_airport,
+        country: destination.field_country.title.en,
+        place: destination.title.en,
+        categories: []
+    });
+    for (const category of destination.field_arrival_guide_destination.raw.Categories.en.Category) {
+        data[data.length - 1].categories.push({
+            id: category.Id,
+            name: category.Name
+        });
+    }
+}
+console.log(JSON.stringify(data));
+
+
 const hotels = [
     {
         title: "Test_Hotel_1",
